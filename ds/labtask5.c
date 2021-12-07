1c16a864068609b3d1ba81d60b6cfa2f5ca58c39
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2021-12-07 11:36:34 +0530

    Create labtask5.c

---
+++ ds/labtask5.c
@@ -0,0 +1,78 @@
+// Menu driven program
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int tos = 0;
+int arr[50] ;
+
+void push(int x){
+    if (tos>5){
+        printf("Stack Overflow");
+    }
+    else{
+        tos++;
+        arr[tos] = x;
+    }
+}
+
+int pop(){
+    if(tos<=0){
+        printf("Stack empty");
+    }
+    else{
+        int y = arr[tos];
+        tos--;
+        return y;
+    }
+}
+
+void display(){
+    for(int i=1;i<=tos;i++){
+        printf("%d\n",arr[i]);
+    }
+}
+
+int main(){
+    int key,x;
+    while(1){
+        printf("Enter 1,2,3 or 4\n");
+        printf("1 push\n");
+        printf("2 pop\n");
+        printf("3 display\n");
+        printf("4 stop\n");
+        scanf("%d",&key);
+
+        switch(key){
+            case 1:
+                if (tos<=5){
+                    printf("Enter an element to push : ");
+                    scanf("%d",&x);
+                    push(x);
+                }
+                else{
+                    printf("Stack Overflow\n");
+                }
+                break;
+            case 2:
+                if(tos!=0){
+                    printf("Poped element is : %d\n",pop());
+                }
+                else{
+                    printf("Stack is empty\n");
+                }
+                break;
+            
+            case 3:
+                display();
+                break;
+            case 4:
+                exit(0);
+                break;
+            
+        }
+    }
+
+    
+    return 0;
+}
