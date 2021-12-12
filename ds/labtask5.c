fa9652836941ef0990d3047f786453865a865e6b
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2021-12-12 15:46:21 +0530

    Update labtask5.c

--- ds/labtask5.c
+++ ds/labtask5.c
@@ -78,16 +78,23 @@ int main(){
 }
 
 
+
+
+
+
 // task2
 
-#include <stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
-char arr[50] ;
+#include<string.h>
+
 int tos = 0;
+char arr[50] ;
+
 void push(char x){
-    if (tos>=5){
+    if (tos>5){
         printf("Stack Overflow");
-    {
+    }
     else{
         tos++;
         arr[tos] = x;
@@ -99,26 +106,63 @@ int pop(){
         printf("Stack empty");
     }
     else{
-        int y = arr[tos];
+        char y = arr[tos];
         tos--;
         return y;
     }
 }
 
+void display(){
+    for(int i=1;i<=tos;i++){
+        printf("%d\n",arr[i]);
+    }
+}
 
-int main()
-{
-   char str[20];
-   gets(str);
-   printf("%s", str);
-//   printf("%ld",strlen(str));/
-    for(int i=0;i<strlen(str);i++){
-        push(str[i]);
+int main(){
+    int check,key;
+    char word[50];
+    
+    // printf("%s", word);
+    // printf("\nName length: %u", length);
+    
+    while(1){
+        printf("1.Check string is palindrome\n");
+        printf("2.exit\n");
+        printf("Choose Operation : ");
+        scanf("%d",&key);
+        
+        switch(key){
+            case 1:
+                printf("Enter string : ");
+                scanf("%s",word);
+                
+                int length = strlen(word);
+                
+                for(int i=0;i<length;i++){
+                    push(word[i]);
+                }
+    
+                for(int i=0;i<length;i++){
+                    char a = pop();
+                    if (a != word[i]){
+                        check = 0;
+                        break;
+                    }
+                }
+                
+                if (check==0){
+                    printf("%s is not a palindrome\n",word);
+                }
+                else{
+                    printf("%s is a palindrome\n",word);
+                }
+                break;
+                
+            case 2:
+                exit(0);
+        }
+        
     }
     
-    for(int i=0;i<strlen(str);i++){
-        printf("%s",arr[i]);
-    }
-   
-   return 0;
+    return 0;
 }
