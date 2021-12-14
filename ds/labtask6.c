1d6ffbacab09842d90d8e88a79bc7d5403d414cc
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2021-12-14 11:52:29 +0530

    Create labtask6.c

---
+++ ds/labtask6.c
@@ -0,0 +1,162 @@
+// Task 1
+
+#include <stdio.h>
+#include <stdlib.h>
+//Queue Operations
+
+int front = 0;
+int rear = -1;
+int max = 3;
+int a[50];
+
+void insert(int x){
+    if (rear >= max){
+        printf("Queue Overflow\n\n");
+    }
+    else{
+        rear++;
+        a[rear] = x;
+    }
+}
+
+void delete(){
+    if(front == rear || rear == 0){
+        printf("Queue is empty\n");
+    }
+    else{
+        int y = a[front];
+        front++;
+        printf("The deleted element is : %d\n\n",y);
+    }
+
+}
+
+void display(){
+    if(front == rear || rear == 0){
+        printf("Queue is empty\n");
+    }
+    else{
+        for(int i=front;i<=rear;i++){
+            printf("%d ",a[i]);
+        }
+    }
+}
+
+int main()
+{
+    int key,e,z;
+    while(1){
+        printf("1.Insert\n2.Delete\n3.Display\n4.exit\n");
+        printf("Enter your choice : ");
+        scanf("%d",&key);
+        switch(key){
+            case 1:
+                printf("Enter your element to insert : ");
+                scanf("%d",&e);
+                insert(e);
+                break;
+            case 2: 
+                delete();
+                break;
+            case 3:
+                printf("Elements in the  queue are \n");
+                display();
+                printf("\n\n");
+                break;
+            case 4:
+                exit(0);
+                break;
+            default :
+                printf("Enter a valid choice\n");
+                break;
+                
+        }
+    }
+    
+
+    return 0;
+}
+
+
+
+// Task 2
+
+#include <stdio.h>
+#include<string.h>
+
+// Postfix Evaluation
+
+int tos = 0;
+int arr[50] ;
+
+void push(int x){
+    if (tos>=7){
+        printf("Stack Overflow");
+    }
+    else{
+        tos++;
+        arr[tos] = x;
+    }
+}
+
+int pop(){
+    if(tos<=0){
+        printf("Stack empty");
+    }
+    else{
+        int y = arr[tos];
+        tos--;
+        return y;
+    }
+}
+
+
+int main()
+{
+    char word[50];
+    int val1,val2,ans,x,y;
+    
+    scanf("%s", word);
+    int length = strlen(word);
+    
+    // printf("%s",word);
+    for(int i=0;i<length;i++){
+        // if(word[i] == "+" || word[i] == "-" || word[i] == "*" ||word[i] == "/" ){
+        if(word[i]-'0' >=0 || word[i]-'0' <=9 ){
+            printf("%d",word[i]);
+            push(word[i]);
+        }
+        else{
+            val2 = pop()-'0';
+            val1 = pop()-'0';
+            printf("%d  %d",val1,val2);
+            x = val1;
+            y = val2;
+            switch(word[i]){
+                
+                case '+':
+                    push(x+y);
+                    break;
+                    
+                case '-':
+                    push(x-y);
+                    break;
+                    
+                case '*':
+                    push(x*y);
+                    break;
+                
+                case '/':
+                    push(x/y);
+                    break;
+            }
+        }
+        
+        
+    }
+    
+    printf("ans is : %d\n",arr[0]);
+
+// 53+82-*
+    return 0;
+}
