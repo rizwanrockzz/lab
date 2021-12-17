2f85f8fffc9c5f0071ba5a7c07a583138fce86ce
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2021-12-17 21:46:14 +0530

    Update operatoroverloading.cpp

--- cpp/operatoroverloading.cpp
+++ cpp/operatoroverloading.cpp
@@ -7,7 +7,7 @@ public:
     int a;
     
     void get(){
-        cout<<"Enter : ";
+        cout<<"Enter a value : ";
         cin>>a;
     }
     
@@ -31,6 +31,34 @@ public:
         cout<<"Remainder is : "<<a%l.a<<endl;
     }
     
+    void operator >(Load l){
+        if(a>l.a){
+            cout<<"Object 1 is greater than Object 2";
+        }
+        else{
+            cout<<"Object 2 is greater than Object 1";
+        }
+    }
+    
+    void operator <(Load l){
+
+        if(a<l.a){
+            cout<<"Object 1 is less than Object 2";
+        }
+        else{
+            cout<<"Object 2 is less than Object 1";
+        }
+    }
+    
+    void operator ==(Load l){
+        if(a==l.a){
+            cout<<"Object 1 and Object 2 both are equal";
+        }
+        else{
+            cout<<"Object 1 and Object 2 both are not equal";
+        }
+    }
+    
 };
 
 int main()
@@ -39,8 +67,7 @@ int main()
     l1.get();
     l2.get();
     char c;
-    // cout<<"1"
-    cout<<"Enter operator : ";
+    cout<<"Enter the operator which you want to overload : ";
     cin>>c;
     switch (c){
         case '+':
@@ -55,16 +82,19 @@ int main()
         case '/':
             l1/l2;
             break;
-        case '%':
-            l1%l2;
+        case '>':
+            l1>l2;
+            break;
+        case '<':
+            l1<l2;
+            break;
+        case '=':
+            l1==l2;
             break;
         default:
             cout<<"Enter a valid operator"<<endl;
             break;
     }
         
-
-    
-
     return 0;
 }
