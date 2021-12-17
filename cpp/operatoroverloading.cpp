622ec3e8726129885fe05d7fa48812ccf9d109cf
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2021-12-16 21:34:26 -0800

    Update operatoroverloading.cpp

--- cpp/operatoroverloading.cpp
+++ cpp/operatoroverloading.cpp
@@ -7,12 +7,30 @@ public:
     int a;
     
     void get(){
+        cout<<"Enter : ";
         cin>>a;
     }
     
     void operator +(Load l){
-        cout<<"sum is : "<<a+l.a<<endl;
+        cout<<"Sum is : "<<a+l.a<<endl;
     }
+    
+    void operator -(Load l){
+        cout<<"Subtraction is : "<<a-l.a<<endl;
+    }
+    
+    void operator *(Load l){
+        cout<<"Product is : "<<a*l.a<<endl;
+    }
+    
+    void operator /(Load l){
+        cout<<"Division is : "<<a/l.a<<endl;
+    }
+    
+    void operator %(Load l){
+        cout<<"Remainder is : "<<a%l.a<<endl;
+    }
+    
 };
 
 int main()
@@ -20,8 +38,32 @@ int main()
     Load l1,l2;
     l1.get();
     l2.get();
-    // cout<<l1+l2<<endl;
-    l1+l2;
+    char c;
+    // cout<<"1"
+    cout<<"Enter operator : ";
+    cin>>c;
+    switch (c){
+        case '+':
+            l1+l2;
+            break;
+        case '-':
+            l1-l2;
+            break;
+        case '*':
+            l1*l2;
+            break;
+        case '/':
+            l1/l2;
+            break;
+        case '%':
+            l1%l2;
+            break;
+        default:
+            cout<<"Enter a valid operator"<<endl;
+            break;
+    }
+        
+
     
 
     return 0;
