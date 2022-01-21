8c6efafeff6c8deb5b92f69e63f1361af57bc423
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2022-01-21 11:44:48 +0530

    Update template.cpp

--- cpp/template.cpp
+++ cpp/template.cpp
@@ -21,16 +21,12 @@ using namespace std;
 
 template<typename T>
     void bubblesort(T arr,int n){
-        int counter = 1;
-        while (counter<n){
-            for(int i=0;i<(n-counter);i++){
-                if (arr[i]>arr[i+1]){
-                    swap(arr[i],arr[i+1]);
-                }
-            }
-            counter++;
-        }
-        
+        for (int i = 0; i < n-1; i++){
+            for (int j = 0; j < n-i-1; j++) 
+                if (arr[j] > arr[j+1]) 
+                    swap(arr[j], arr[j+1]);
+        }    
+            
     }
 
 
