9ce3c7fbb64b077bc28516a12e6c8719d016c0e4
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2022-01-21 09:37:55 +0530

    Create week9.cpp

---
+++ cpp/week9.cpp
@@ -0,0 +1,47 @@
+//  PROGRAM 1
+
+#include <iostream>
+using namespace std;
+
+class Shape{
+protected:
+	double a,b;
+public:
+	virtual void display() = 0;
+	void getdata(){
+		cout<<"Entert dimension 1 : ";
+		cin>>a;
+		cout<<"Entert dimension 2 : ";
+		cin>>b;
+	}
+
+};
+
+class Triangle:public Shape{
+public:
+	void display(){
+		cout<<"Area of triangle is : "<<0.5*a*b;
+	}
+
+};
+
+class Rectangle:public Shape{
+public:
+	void display(){
+		cout<<"Area of rectangle is : "<<a*b;
+	}
+
+};
+
+int main(){
+	Triangle t;
+	Rectangle r;
+	cout<<"Enter the dimensions for triangle\n\n";
+	t.getdata();
+	t.display();
+	cout<<"Enter the dimensions for rectangle\n\n";
+	r.getdata();
+	r.display();
+
+	return 0;
+}
