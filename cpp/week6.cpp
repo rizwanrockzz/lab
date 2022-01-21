bd88cbf2f29c4d58fe5ea837179041bf05459e1a
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2022-01-22 00:16:57 +0530

    Update week6.cpp

--- cpp/week6.cpp
+++ cpp/week6.cpp
@@ -105,3 +105,92 @@ int main()
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the no of operations : ";
+    cin>>n;
+    int j=0;
+    stack<int> st;
+	char op[20];
+	int arr[4];
+	int max[n];
+	for(int i=0;i<=n;i++){
+	    cin.getline(op,20);
+
+    	char * token;
+    
+    	token = strtok (op," ");
+    	while (token != NULL)
+        {
+    		string abc(token);
+    		int a = stoi(abc);
+    		arr[j] = a;
+    		token = strtok (NULL, " ");
+    		j+=1;
+        }
+      
+      
+      
+    //   for(int i=0;i<j;i++){
+    //       cout<<arr[i]<<"    ";
+    //   }
+    
+    if(arr[0]==1){
+        st.push(arr[1]);
+        max[i]=arr[1];
+    }
+    else if(arr[0]==2){
+        st.pop();
+    }
+    
+    else if(arr[0]==3){
+        cout<<max(maxele)<<endl;
+    }
+      
+    for(int i=0;i<j;i++){
+      arr[i] = 0;
+    }
+      
+
+      
+      j=0;
+      
+      cout<<endl;
+	}
+	
+	return 0;
+}
