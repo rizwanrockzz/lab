b8b7cd84e52c7666eb8d5d87fe559b8f0ac1f1dd
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2022-02-16 21:25:05 +0530

    Update labtask10.c

--- ds/labtask10.c
+++ ds/labtask10.c
@@ -151,5 +151,125 @@ int main()
 
 
 
+// Task-3
+/*
+Based on the knowledge acquired from Task-1 and Task-2. Perform (i) Insert of node at the end of 
+a double linked list and (ii) Insert of node at the specified position of a double linked list.
+*/
+
+#include<stdio.h>  
+#include<stdlib.h> 
+struct node 
+{ 
+    struct node *prev; 
+    struct node *next; 
+    int data; 
+}*head; 
+
+void insertion_last() 
+{ 
+   struct node *new,*temp; 
+   int item; 
+   new = (struct node *) malloc(sizeof(struct node)); 
+   printf("Enter value : "); 
+   scanf("%d",&item); 
+   new->data=item; 
+   if(head == NULL) 
+   { 
+       new->next = NULL; 
+       new->prev = NULL; 
+       head = new; 
+   } 
+   else 
+   { 
+      temp = head; 
+      while(temp->next!=NULL) 
+      { 
+      	temp = temp->next; 
+      } 
+      temp->next = new; 
+      new ->prev=temp; 
+      new->next = NULL; 
+      } 
+        
+} 
+      
+void insertion_specified() 
+{ 
+   struct node *new,*temp; 
+   int item,loc,i; 
+   new = (struct node *)malloc(sizeof(struct node)); 
+ 
+   temp=head; 
+   printf("Enter the location"); 
+   scanf("%d",&loc); 
+   for(i=0;i<loc;i++) 
+   { 
+       temp = temp->next; 
+       if(temp == NULL) 
+       { 
+       	printf("\n There are less than %d elements", loc); 
+       	return; 
+       } 
+   } 
+   printf("Enter value"); 
+   scanf("%d",&item); 
+   new->data = item; 
+   new->next = temp->next; 
+   new-> prev = temp; 
+   temp->next = new; 
+   temp->next->prev=new; 
+       
+  
+} 
+ 
+void display() 
+{ 
+    struct node *p; 
+    printf("\n printing values...\n"); 
+    p = head; 
+    while(p != NULL) 
+    { 
+        printf("%d\n",p->data); 
+        p=p->next; 
+    } 
+}
+ 
+void main () 
+{ 
+int choice =0; 
+ printf("1.Insert at last\n2.Insert at any random location\n3.Show\n4.Exit\n");
+    while(1) 
+    { 
+        printf("Enter your choice : "); 
+        scanf("%d",&choice); 
+        switch(choice) 
+        { 
+            case 1: 
+                insertion_last(); 
+                break; 
+            case 2: 
+                insertion_specified(); 
+                break; 
+            case 3: 
+                display(); 
+    	        break; 
+            case 4: 
+                exit(0); 
+            default: 
+                printf("Please enter valid choice..\n"); 
+        } 
+    } 
+} 
+ 
+   
+
+
+
+
+
+
+
+
 
 
