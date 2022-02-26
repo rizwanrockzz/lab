9c5eab38ccfcbd212e2bc0cacfdcb053a5991060
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2022-02-26 20:41:38 +0530

    Update stl.cpp

--- cpp/stl.cpp
+++ cpp/stl.cpp
@@ -113,5 +113,69 @@ int main()
 
 
 
+// 4th task 
+
+#include <deque>
+#include <iostream>
+using namespace std;
+
+void showdq(deque<int> g)
+{
+	deque<int>::iterator it;
+	for (it = g.begin(); it != g.end(); ++it)
+		cout << '\t' << *it;
+	cout << '\n';
+}
+
+int main()
+{
+	deque<int> gquiz;
+	gquiz.push_back(10);
+	gquiz.push_front(20);
+	gquiz.push_back(30);
+	gquiz.push_front(15);
+	cout << "The deque gquiz is : ";
+	showdq(gquiz);
+
+	cout << "\ngquiz.size() : " << gquiz.size()<<endl;
+	cout << "\ngquiz.max_size() : " << gquiz.max_size()<<endl;
+
+	cout << "\ngquiz.at(2) : " << gquiz.at(2)<<endl;
+	cout << "\ngquiz.front() : " << gquiz.front()<<endl;
+	cout << "\ngquiz.back() : " << gquiz.back()<<endl;
+
+	cout << "\ngquiz.pop_front() : ";
+	gquiz.pop_front();
+	showdq(gquiz);
+
+	cout << "\ngquiz.pop_back() : ";
+	gquiz.pop_back();
+	showdq(gquiz);
+
+	return 0;
+}
+
+// output:
+// The deque gquiz is :    15      20      10      30
+
+// gquiz.size() : 4
+
+// gquiz.max_size() : 2305843009213693951
+
+// gquiz.at(2) : 10
+
+// gquiz.front() : 15
+
+// gquiz.back() : 30
+
+// gquiz.pop_front() :     20      10      30
+
+// gquiz.pop_back() :      20      10
+
+
+
+
+
+
 
 
