75611820aace96659c1dc46824f375805215e488
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2022-03-30 11:07:19 +0530

    Add files via upload

---
+++ R/firstR.R
@@ -0,0 +1,124 @@
+# Basics Of R
+
+4+5
+
+1+2+3
+
+3*7*2
+
+4/2
+
+4/3
+
+4*6+5
+
+(4*6)+5
+
+4*(6+5)
+
+# Variable Declaration
+
+a = 11
+b <- 7
+b
+9 -> c
+c
+
+d <- e <- 15
+d
+e
+
+assign("f",19)
+f
+rm(f)
+f
+
+g <- 8L
+g
+
+# Data Types
+
+class(a)
+class(f)
+
+is.numeric(b)
+
+is.integer(g)
+
+class(4L)
+
+class(2.8)
+
+
+class(4L * 2.8)
+
+class(5L)
+
+class(2L)
+
+5L/2L
+
+class(5L/2L)
+
+x <- "rizwan"
+x
+
+nchar(x)
+
+nchar('hello')
+
+nchar(3)
+
+nchar(452)
+
+
+datetoday<-as.Date('2020-03-30')
+
+
+class(datetoday)
+
+as.numeric(datetoday)
+
+date<-as.POSIXct('2020-03-30 11:05')
+date
+
+class(date)
+
+as.numeric(date)
+
+
+class(date)
+
+class(as.numeric(date))
+
+
+2 < 3
+2 > 10
+
+"apple">"banana"
+
+"dog" > "Cat"
+
+2 == 2
+
+2!=7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
