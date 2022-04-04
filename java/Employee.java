21f611df654c68f5c436a2df15e59728744d6dd6
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2022-04-04 10:36:07 +0530

    Create Employee.java

---
+++ java/Employee.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter employee number : ");
+		int emp_no = sc.nextInt();
+		System.out.print("Enter employee name : ");
+		String emp_name = sc.next();
+		System.out.print("Enter contact of employee : ");
+		long contact = sc.nextLong();
+		System.out.print("Enter the employee salary : ");
+		long salary = sc.nextLong();
+		
+		System.out.println("The name of the employee is : "+emp_name);
+		System.out.println("The no of employee is : "+emp_no);
+		System.out.println("The salary of the employee is : "+salary);
+		System.out.println("The contact of employee is : "+contact);
+		
+	}
+}
