9731cccae29a496a3d13551f56d8ce18d62d56e1
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2022-04-06 10:44:19 +0530

    Add files via upload

---
+++ R/week2.R
@@ -0,0 +1,119 @@
+# vectors
+
+x = c(1,2,3,4,5,6,7,8)
+x
+
+x+1
+
+x^10
+
+sqrt(x)
+
+class(x)
+
+# Colon Operator
+
+1:5
+
+5:1
+
+-1:5
+
+5:-1
+
+a = 1:7
+b = -5:1
+a
+b
+
+a+b
+
+a/b
+
+0/100
+
+100/0
+
+0/0
+
+c = 1:4
+d = 2:5
+
+c^d
+
+a
+b
+c
+d
+
+a+c
+b+d
+
+
+length(a)
+
+length(a+b+c+d)
+
+a+c(0.1,0.2,0.3)
+
+
+a<3
+b>6
+
+d<c
+
+# any & all inbuilt functions
+
+any(a>1)
+any(c>100)
+
+all(d>10)
+
+
+nchar(q)
+
+nchar(a)
+
+a
+
+a[4]
+
+a[2:4]
+
+a[c(1,5)]
+
+e = c(34,12,67,34,11,21,67,89)
+e
+
+e[c(3,6)]
+
+d
+
+names(d) = c("col1","col2","col3","col4")
+d
+
+
+
+q1<- c("Hockey", "Football", "Baseball", "Curling", "Rugby")
+q1
+
+
+q2 = c(q1,"Hockey", "Lacrosse", "Hockey", "Water Polo", "Hockey", "Lacrosse")    
+q2
+
+qfactor = as.factor(q2)
+qfactor
+
+as.numeric(qfactor)
+
+
+
+
+
+
+
+
+
+
+
+
