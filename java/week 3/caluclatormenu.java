ead4f0adb68012c4de4155776138dc31d95daf57
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2022-04-18 10:03:43 +0530

    Update caluclatormenu.java

--- java/week 3/caluclatormenu.java
+++ java/week 3/caluclatormenu.java
@@ -1,6 +1,6 @@
 package javalabrizwan99;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class caluclatormenu {
     public static void main(String args[]){
@@ -8,29 +8,34 @@ public class caluclatormenu {
         char key;
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Enter a : ");
-        a = sc.nextInt();
-        System.out.print("Enter b : ");
-        b = sc.nextInt();
-        System.out.print("Enter your operation (Eg : +,-,*,/) : ");
-        key = sc.next().charAt(0);
-        
-        switch(key){
-            case '+':
-                System.out.println(a+b);
-                break;
-            case '-':
-                System.out.println(a-b);
-                break;
-            case '*':
-                System.out.println(a*b);
-                break;
-            case '/':
-                System.out.println(a/b);  
-                break;
-            default:
-                System.out.println("Enter a valid symbol");  
+        System.out.println("To exit enter !");
+        while(true){
+            System.out.print("Enter a : ");
+            a = sc.nextInt();
+            System.out.print("Enter b : ");
+            b = sc.nextInt();
+            System.out.print("Enter your operation (Eg : +,-,*,/) : ");
+            key = sc.next().charAt(0);
+            switch(key){
+             case '+':
+                 System.out.println(a+b);
+                 break;
+             case '-':
+                 System.out.println(a-b);
+                 break;
+             case '*':
+                 System.out.println(a*b);
+                 break;
+             case '/':
+                 System.out.println(a/b);  
+                 break;
+             case '!':
+                 System.exit(0);
+             default:
+                 System.out.println("Enter a valid symbol");  
+             } 
         }
+        
         
     }
 }
