c93bcacb35914a52a3502788b8d79e46776debf9
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2022-04-25 09:10:00 +0530

    Create array1d.java

---
+++ java/week 4/array1d.java
@@ -0,0 +1,12 @@
+package rizwan99;
+
+public class array1d {
+    public static void main(String args[]){
+        int a[] = {10,20,30,40,50};
+        System.out.print("Given Array : ");
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+    }
+}
