46a5590423698a983a81e1cef48f2c27f2b33f78
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2022-05-16 10:00:11 +0530

    Create constructorsAndMethods.java

---
+++ java/week 7/constructorsAndMethods.java
@@ -0,0 +1,51 @@
+package rizwanjava;
+
+class Employee{
+    int sno,smarks;
+    String sname;
+    
+    // Default constructor
+    Employee(){
+        sno = 1;
+        sname = "mrname";
+        smarks = 99;
+    }
+       
+    // Parameterized constructor
+    Employee(int sno,int smarks,String sname){
+        this.sno = sno;
+        this.sname = sname;
+        this.smarks = smarks;
+    }
+    
+    // Default Method
+    void setDefault(){
+        sno = 3;
+        sname = "rockzz";
+        smarks = 92;
+    }
+    
+    // Parameterized Method
+    void setFields(int a,int b,String c){
+        sno = a;
+        smarks = b;
+        sname = c;
+    }
+    
+    void display(){
+        System.out.println("Sno is : "+sno);
+        System.out.println("sname is : "+sname);
+        System.out.println("smarks is : "+smarks);
+    }
+}
+
+
+public class constructorsAndMethods {
+    public static void main(String args[]){
+        Employee emp = new Employee();
+//        Employee emp = new Employee(2,99,"rizwan");
+//        emp.setDefault();
+        emp.setFields(4, 94, "rizwan rockzz");
+        emp.display();
+    }
+}
