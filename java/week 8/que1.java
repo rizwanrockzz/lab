9722eb3fc78538ff45fb4b86e9beea5eeb3e97ee
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2022-06-20 11:06:15 +0530

    Update que1.java

--- java/week 8/que1.java
+++ java/week 8/que1.java
@@ -1,3 +1,6 @@
+// Create a class A with the variables x,y. Create a method to set the date to x and y. 
+// Create a subclass B with the variable z. create a method to set the data to z. Write a method to display the information.
+
 package pkg208w1a1299.inheritance;
 
 class A{
