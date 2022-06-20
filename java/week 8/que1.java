70cd540e0030c8e784f4f07b55a491b834aba83a
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2022-06-20 10:20:08 +0530

    Create que1.java

---
+++ java/week 8/que1.java
@@ -0,0 +1,30 @@
+package pkg208w1a1299.inheritance;
+
+class A{
+    int x,y;
+    void setA(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class B extends A{
+    int z;
+    void setB(int z){
+        this.z = z;
+    }
+    void display(){
+        System.out.println("x : "+x);
+        System.out.println("y : "+y);
+        System.out.println("z : "+z);
+    }
+}
+
+public class que1 {
+    public static void main(String args[]){
+        B b = new B();
+        b.setA(10, 20);
+        b.setB(30);
+        b.display();
+    }
+}
